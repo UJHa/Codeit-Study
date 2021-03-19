ad24d53bcd87bf3141bf8d02a64abc0cc86bf934
ssung <33725048+parkseonga@users.noreply.github.com>
2021-03-19 23:41:09 +0900

    Update seonga.py

--- (07) 문제가 너무 쉬워요 - List와 String/03) 1292 쉽게 푸는 문제/seonga.py
+++ (07) 문제가 너무 쉬워요 - List와 String/03) 1292 쉽게 푸는 문제/seonga.py
@@ -1,44 +1,12 @@
-## 1. 8*8 체스판을 만든다.
-## 2. 흰 칸의 인덱스를 받는다.
-## 3. 흰 칸 인덱스에 F가 몇 개 있는지 확인한다. 
-#----------------------------------------------
-# 입력 받기 
-arr_input = [list(input()) for _ in range(8)]
+# 입력받기
+n, m = map(int, input().split())
 
-# 체스판 만들기 
-arr = [[-1]*8 for _ in range(8)]
-
-i = 0
-even = ['W','B']*4
-odd = ['B','W']*4
-
-while i<len(arr):
-    if i%2 == 0: # 짝수이면
-        arr[i] = even
-    else:
-        arr[i] = odd
-        
-    i += 1
-    
-# 2. 흰 칸 위에 말이 몇 개 있는지 확인하기     
-row = -1
-
-row_index = []
-col_index = []
-
-for i in arr:
-    row +=1
-    col = -1
-    for a in i:
-        col += 1
-        if a=="W":
-            row_index.append(row)
-            col_index.append(col)
-
+# 122333444 와 같은 수열 생성하기 
+arr = []
 cnt = 0
 
-for row_index_value, col_index_value in zip(row_index, col_index):
-    if arr_input[row_index_value][col_index_value] == 'F':
-        cnt +=1
-        
-print(cnt)
+while len(arr)<m: 
+    cnt += 1 
+    arr.extend([cnt]*cnt)  # cnt 값을 반복해서 수열 생성 
+
+print(sum(arr[n-1:m]))  # n, m 까지 범위의 숫자 합 구함.
