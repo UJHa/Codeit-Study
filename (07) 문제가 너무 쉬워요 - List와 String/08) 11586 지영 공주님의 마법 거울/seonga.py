f6cdfa522009d81ed1ea934a5bf06be34f3f1f02
ssung <33725048+parkseonga@users.noreply.github.com>
2021-03-21 03:04:30 +0900

    Upload seonga.py

---
+++ (07) 문제가 너무 쉬워요 - List와 String/08) 11586 지영 공주님의 마법 거울/seonga.py
@@ -0,0 +1,42 @@
+n = int(input())
+arr = [list(input()) for _ in range(n)]  
+emotion = int(input())
+
+
+import copy
+
+i = 0
+j = len(arr)-1
+
+temp = copy.deepcopy(arr)
+
+
+if emotion == 1:
+    print(arr)
+    
+    
+elif emotion == 2:
+    
+
+    while i < len(arr)-(n//2):
+            
+        for row in range(len(arr)):
+            
+            arr[row][i] = temp[row][j]
+            arr[row][j] = temp[row][i]
+                    
+        i += 1
+        j -= 1
+            
+    print(arr)
+            
+elif emotion == 3:
+    while i < len(arr)-(n//2):
+       
+        arr[i] = temp[j]
+        arr[j] = temp[i]
+                
+        i += 1
+        j -= 1
+        
+    print(arr)
