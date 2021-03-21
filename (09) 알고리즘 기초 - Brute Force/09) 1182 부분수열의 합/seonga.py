364ebe2234ed1997c77bbca9cdb544f6ea5c666f
ssung <33725048+parkseonga@users.noreply.github.com>
2021-03-22 02:30:24 +0900

    Update seonga.py

--- (09) 알고리즘 기초 - Brute Force/09) 1182 부분수열의 합/seonga.py
+++ (09) 알고리즘 기초 - Brute Force/09) 1182 부분수열의 합/seonga.py
@@ -1,12 +1,13 @@
-from itertools import combinations
+from itertools import combinations  # combinations 조합 모듈 (순열 모듈도 존재)
 
 def partitional_sequence(n, sum_m, lst):
     
     count = 0
     
     for i in range(1, len(lst)+1):
-        lst_com = list(combinations(lst,i))
-        for list_value in lst_com:
+        lst_com = list(combinations(lst,i))  # lst에서 i개 만큼 뽑는 모든 경우의 수 추출 
+        
+        for list_value in lst_com:   # 추출된 경우의 수 반복적으로 돌면서 합 확인 
             if sum(list_value) == sum_m:
                 count += 1
                 
