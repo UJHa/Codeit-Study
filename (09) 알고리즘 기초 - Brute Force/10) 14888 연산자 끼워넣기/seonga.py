2563080942ddec901ac33825070d2be3ac440451
ssung <33725048+parkseonga@users.noreply.github.com>
2021-03-22 19:52:32 +0900

    not yet

--- (09) 알고리즘 기초 - Brute Force/10) 14888 연산자 끼워넣기/seonga.py
+++ (09) 알고리즘 기초 - Brute Force/10) 14888 연산자 끼워넣기/seonga.py
@@ -1,21 +1,23 @@
+from itertools import permutations
+
 def candy_operator(n, arr, op_arr):
     
     lst = []
     
-    for i in range(4): # 0: +, 1: -, 2: *, 3: //  -> 순서가 있으므로 순열 사용 
+    for i in range(4): # 0: +, 1: -, 2: *, 3: //  
         for j in range(op_arr[i]):
             lst.append(i)  # 연산자 리스트 생성  
             
-    from itertools import permutations
-    
     operator = set(list(permutations(lst, 2)))  # 연산자 추출 후 중복 제거 
     
     # 항상 -10억보다 크거나 같고 10억보다 작거나 같은 결과가 나오는 입력만 주어짐. 
-    max_value = 10000000
-    min_value = -10000000
+    max_value = -10000000
+    min_value = 10000000
     
     for candy_op in operator:
+        
         answer = arr[0]  # 주어진 수의 순서를 바꾸면 안됨. 
+        
         for i in range(len(candy_op)):
             if candy_op[i] == 0:
                 answer = answer + arr[i+1]
@@ -39,3 +41,4 @@ max_, min_ = candy_operator(n, num_arr, op_arr)
   
 print(max_)
 print(min_)   
+
