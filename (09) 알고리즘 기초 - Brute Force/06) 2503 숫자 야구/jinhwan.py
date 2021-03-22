9f365f55706470c977593dfc8be8d36962666a1d
UHA <ddkk94@naver.com>
2021-03-22 23:23:19 +0900

    2503 숫자야구 완료
    Brute Force
    (2503) 숫자야구 - 완료
    
    Co-Authored-By: LeeJunhee <45219806+LanguidCat@users.noreply.github.com>

--- (09) 알고리즘 기초 - Brute Force/06) 2503 숫자 야구/jinhwan.py
+++ (09) 알고리즘 기초 - Brute Force/06) 2503 숫자 야구/jinhwan.py
@@ -1,9 +1,28 @@
 # (2503) 숫자 야구
-# 푸는 중...(아직 다 풀지 않는 문제)
+# ljh.py 참고했습니다.
+def check_strike(question_num, temp_num):
+    result = 0
+    for i in range(3):
+        if question_num[i] == temp_num[i]:
+            result += 1
+    return result
+
+
+def check_ball(question_num, temp_num):
+    result = 0
+
+    if question_num[0] == temp_num[1] or question_num[0] == temp_num[2]: result += 1
+    if question_num[1] == temp_num[2] or question_num[1] == temp_num[0]: result += 1
+    if question_num[2] == temp_num[0] or question_num[2] == temp_num[1]: result += 1
+
+    return result
+
+
 n = int(input())
-#3s, 1s 2b, 3b, 2s, 1s 1b, 0s 2b, 1s, 1b
+
+questions = []
 for i in range(n):
-    questions = list(map(int, input().split()))
+    questions.append(list(map(int, input().split())))
 
 all_numbers = {}
 for i in range(1, 10):
@@ -15,16 +34,18 @@ for i in range(1, 10):
                 continue
             all_numbers[str(i)+str(j)+str(k)] = True
 
-print(all_numbers)
-
 for question in questions:
     question_number, strike, ball = str(question[0]), question[1], question[2]
-    if strike + ball == 3:
-        pass
-    elif strike + ball == 2:
-        pass
-    elif strike + ball == 1:
-        pass
-    elif strike + ball == 0:
-        pass
+    for num in all_numbers:
+        s = check_strike(question_number, num)
+        b = check_ball(question_number, num)
+        if s != strike or b != ball:
+            all_numbers[num] = False
+
+output_value = 0
+for key, val in all_numbers.items():
+    if val == True:
+        output_value += 1
+
+print(output_value)
 
