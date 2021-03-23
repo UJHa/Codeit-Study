1ac4b22b04db03ced4c80a25bcf5451050693e2a
UHA <ddkk94@naver.com>
2021-03-23 22:13:57 +0900

    체스판 다시 칠하기 문제 해결
    (1018) 체스판 다시 칠하기 - 완료
    원인 : 시작하는 좌표의 색상에 따라서 첫 번째 원소는 반드시 옳은 색상으로 판단하도록 구현
    해결 : 첫 번째 원소가 흰색, 검은 색으로 시작하는 모든 경우에 대해서 색칠하게 되는 최소 값을 구하도록 변경
    
    Co-Authored-By: junhyoung <66052461+jjuun0@users.noreply.github.com>

--- (09) 알고리즘 기초 - Brute Force/05) 1018 체스판 다시 칠하기/jinhwan.py
+++ (09) 알고리즘 기초 - Brute Force/05) 1018 체스판 다시 칠하기/jinhwan.py
@@ -1,20 +1,20 @@
 # (1018) 체스판 다시 칠하기
-# 푸는 중...(아직 오답)
-def redraw_count(board, pos_x, pos_y):
+def redraw_count(board, pos_x, pos_y, start_color):
     wrong_count = 0
 
-    start_color = board[pos_y][pos_x]
     correct_color = start_color
 
+    # 입력받은 시작 좌표를 통해서 8x8 크기의 체스판의 각 칸 흑백 색깔 확인
     for y in range(pos_y, pos_y + 8):
         for x in range(pos_x, pos_x + 8):
             if board[y][x] != correct_color:
                 wrong_count += 1
 
+            # 가로(x 이동) 칸 이동 시 색을 번갈아서 체크
             correct_color = 'B' == correct_color and 'W' or 'B'
 
-        start_color = 'W' == start_color and 'B' or 'W'
-        correct_color = start_color
+        # 세로(y 이동) 칸 이동 시 색을 번갈아서 체크
+        correct_color = 'B' == correct_color and 'W' or 'B'
 
     return wrong_count
 
@@ -25,9 +25,12 @@ chess_board = []
 for i in range(height):
     chess_board.append(list(input()))
 
+# 8x8의 체스판에서 최대 수정 값인 64로 비교를 해야 최소로 그리는 횟수를 알 수 있습니다.
 min_value = 64
 for start_pos_y in range(height - 7):
     for start_pos_x in range(width - 7):
-        min_value = min(min_value, redraw_count(chess_board, start_pos_x, start_pos_y))
+        min_value = min(min_value,
+                        redraw_count(chess_board, start_pos_x, start_pos_y, 'W'),
+                        redraw_count(chess_board, start_pos_x, start_pos_y, 'B'))
 
 print(min_value)
