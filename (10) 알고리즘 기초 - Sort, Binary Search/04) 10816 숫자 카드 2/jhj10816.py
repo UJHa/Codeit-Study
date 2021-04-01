8f5c8bc1cd2ca15026d1a5b6484cdd05b695849b
sumb-10 <saku07891@gmail.com>
2021-04-01 23:43:33 +0900

    4/1 jhj1654 not solved

--- (10) 알고리즘 기초 - Sort, Binary Search/04) 10816 숫자 카드 2/jhj10816.py
+++ (10) 알고리즘 기초 - Sort, Binary Search/04) 10816 숫자 카드 2/jhj10816.py
@@ -1,6 +1,24 @@
 '''
-
+not yet solved
+overtake time
+1. card sort
+2. card 정리하고 몇 장인지 리스트로 만들기
+3. 이진탐색으로 정리한 리스트에서 찾기
+4. 개수 몇장인지 정리한 리스트에서 뽑아서 정답 리스트에 어펜드
 '''
+def quicksort(list_a):
+
+    if len(list_a) <= 1 : return list_a
+    else:
+        pivot = list_a[len(list_a)//2]
+        less_arr, equal_arr, big_arr = [], [], []
+        for i in list_a:
+            if i < pivot: less_arr.append(i)
+            elif i > pivot: big_arr.append(i)
+            else: equal_arr.append(i)
+        return quicksort(less_arr) + equal_arr +quicksort(big_arr)
+
+
 def return_idx_bin_search(list_a, m, low, high):
     if low > high:
         return "a"
@@ -17,41 +35,32 @@ def return_idx_bin_search(list_a, m, low, high):
 def count_card():
 
     n = int(input())
-    card = list(map(int, input().split()))
+    cards = list(map(int, input().split()))
     m = int(input())
     card_to_be_checked = list(map(int, input().split()))
-    card.sort()
-    #print(card)
+    cards = quicksort(cards)
+    #print(cards)
+    card_num_list , numbers_of_card_list = [cards[0]], [1]
+    num = cards[0]
+    idx_numbers_of_card_list = 0
+
+    for card in cards:
+        if num < card:
+            num = card
+            card_num_list.append(num)
+            numbers_of_card_list.append(1)
+            idx_numbers_of_card_list += 1
+        else: numbers_of_card_list[idx_numbers_of_card_list] += 1
+
+    lencardlist = len(card_num_list)
 
     for checked_card in card_to_be_checked:
-        count = 0
-        right_check = 1
-        left_check = 1
+        check = return_idx_bin_search(card_num_list, checked_card, 0, lencardlist - 1)
+        if type(check) == int:
+            print(numbers_of_card_list[check], end=' ')
+        else: print(0, end=' ')
 
-        #print(checked_card)
-
-        if type(return_idx_bin_search(card, checked_card, 0, n - 1)) == int:
-            count += 1
-            idx = return_idx_bin_search(card, checked_card, 0, n - 1)
-            #print(idx)
-            while idx - left_check > -1 and left_check != 0:
-                if card[idx - left_check] == checked_card:
-                    count += 1
-                    left_check += 1
-                else: left_check = 0
-            while idx + right_check < n and right_check != 0:
-                if card[idx + right_check] == checked_card:
-                    count += 1
-                    right_check += 1
-                else: right_check = 0
-
-            print(count, end=' ')
-
-        else: print(count, end=' ')
-
-        #print("#####")
+    return 0
 
 
 count_card()
-
-
