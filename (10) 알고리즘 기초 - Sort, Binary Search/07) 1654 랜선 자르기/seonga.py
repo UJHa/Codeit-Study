e13041e9081a491611c9c49efb167a57413efe65
ssung <33725048+parkseonga@users.noreply.github.com>
2021-04-03 19:19:21 +0900

    Create seonga.py

---
+++ (10) 알고리즘 기초 - Sort, Binary Search/07) 1654 랜선 자르기/seonga.py
@@ -0,0 +1,29 @@
+
+from sys import stdin
+
+_, N = map(int, stdin.readline().split())
+LAN_LST = list(map(int, stdin.readlines()))
+
+start_value = 1   # 0으로 하면 zero division error 발생 
+end_value = sum(LAN_LST)//N  # 전체 랜선의 합을 필요한 랜선의 개수만큼 나눈 값까지 반복하며 탐색
+answer = 0  # 랜선 최대 길이를 담을 변수 
+
+while start_value <= end_value:  
+    
+    mid_value = (start_value + end_value)//2  # 랜선의 최대 길이를 찾을 변수 
+    total_sum  = 0
+    
+    for value in LAN_LST: 
+        total_sum += value//mid_value  # 보유하고 있는 랜선을 각각 자를 랜선으로 나눠지는 개수 합 
+        
+    # 나눠진 후 생기는 랜선의 개수를 필요한 랜선의 개수와 비교
+    if total_sum >= N:  
+        answer = max(answer, mid_value)   # 최대 
+        start_value = mid_value + 1
+            
+    elif total_sum < N:
+        end_value = mid_value -1 
+            
+print(answer)
+            
+
