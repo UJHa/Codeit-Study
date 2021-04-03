7fa334aca3ada0dd43a58941489fe661354aa174
ssung <33725048+parkseonga@users.noreply.github.com>
2021-04-03 19:28:13 +0900

    Update seonga.py

--- (10) 알고리즘 기초 - Sort, Binary Search/05) 2805 나무 자르기/seonga.py
+++ (10) 알고리즘 기초 - Sort, Binary Search/05) 2805 나무 자르기/seonga.py
@@ -1,6 +1,10 @@
-# 입력 
-_, home_N = list(map(int, input().split()))
-TreeLST = list(map(int, input().split()))
+# python3 시간초과
+# pype3로 작동
+
+from sys import stdin
+
+_, home_N = map(int, stdin.readline().split())
+TreeLST = list(map(int, stdin.readline().split()))
 
 # 자를 나무(H)의 최소 길이, 최대길이까지를 start_value, end_value로 둠
 start_value = 1
@@ -20,7 +24,7 @@ while start_value <= end_value:
             total_sum += TreeLST[i] - mid_value
         
     if total_sum >= home_N:
-        answer = mid_value
+        answer = max(answer, mid_value)  # max함수를 적용하지 않아도 최대값이 나옴 -> 정렬된 값을 사용해서 그런듯
         start_value = mid_value + 1
             
     elif total_sum < home_N:
