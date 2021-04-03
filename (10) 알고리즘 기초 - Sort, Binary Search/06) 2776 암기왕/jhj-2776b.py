e5ec68096dd5a5c6a3e12ba0dd4532ecd57787ad
sumb-10 <saku07891@gmail.com>
2021-04-03 20:25:24 +0900

    4/03 jhj2776 modified

--- (10) 알고리즘 기초 - Sort, Binary Search/06) 2776 암기왕/jhj-2776b.py
+++ (10) 알고리즘 기초 - Sort, Binary Search/06) 2776 암기왕/jhj-2776b.py
@@ -1,5 +1,6 @@
 '''
 수 찾기와 동일
+not yet solved
 '''
 
 def binary_search(list_a, m, low, high):
@@ -17,20 +18,21 @@ def binary_search(list_a, m, low, high):
 def compare_memo():
 
     t = int(input())
+    list_n = []
     memo1 = []
     memo2 = []
 
-    for i in range(t):
-        n = int(input())
+    for i in range(t): #memo1, memo2 케이스 입력
+        list_n.append(int(input()))
         memo1.append(list(map(int, input().split())))
         m = int(input())
         memo2.append(list(map(int, input().split())))
 
-    for case in range(t):
+    for case in range(t): #검색
         memo1[case].sort()
 
         for num in memo2[case]:
-            if binary_search(memo1[case], num, low = 0, high = n - 1) > -1:
+            if binary_search(memo1[case], num, low = 0, high = list_n[case] - 1) > -1:
                 print(1)
             else:
                 print(0)
