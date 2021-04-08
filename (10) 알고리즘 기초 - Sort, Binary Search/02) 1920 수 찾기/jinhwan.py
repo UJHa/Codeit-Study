65e912501be7b9140822cf23253cb393c8822ff2
UHA <ddkk94@naver.com>
2021-04-08 16:55:19 +0900

    Sort, Binary Search 복습
    (1920) 수 찾기 - 재귀, 반복 풀이 완료

--- (10) 알고리즘 기초 - Sort, Binary Search/02) 1920 수 찾기/jinhwan.py
+++ (10) 알고리즘 기초 - Sort, Binary Search/02) 1920 수 찾기/jinhwan.py
@@ -1,35 +1,38 @@
 # (1920) 수 찾기
-def is_in_list(num_lst, num):
-    # 문제 풀기 진행중...(시간 초과)
-    # if len(num_lst) <= 2:
-    #     return num in num_lst
-    # else:
-    #     center_index = len(num_lst) // 2
-    #     if num_lst[center_index] < num:
-    #         return is_in_list(num_lst[center_index+1:], num)
-    #     elif num_lst[center_index] > num:
-    #         return is_in_list(num_lst[:center_index-1], num)
-    #     else:
-    #         return True
-    start_pos = 0
-    end_pos = len(num_lst)
-    move_pos = (start_pos + end_pos) // 2
-    while 0 <= move_pos < len(num_lst):
-        if num_lst[move_pos] < num:
-            start_pos = move_pos + 1
-        elif num_lst[move_pos] > num:
-            end_pos = move_pos - 1
+
+# 재귀 풀이
+def is_in_list(num, num_lst, start, end):
+    if start > end:
+        return False
+    else:
+        center_index = (start + end) // 2
+        if num_lst[center_index] < num:
+            return is_in_list(num, num_lst, center_index + 1, end)
+        elif num_lst[center_index] > num:
+            return is_in_list(num, num_lst, start, center_index - 1)
         else:
             return True
-        move_pos = (start_pos + end_pos) // 2
 
-    return False
+# 반복을 통한 풀이
+# def is_in_list(num, num_lst):
+#     start_pos = 0
+#     end_pos = len(num_lst) - 1
+#     move_pos = (start_pos + end_pos) // 2
+#     while start_pos <= end_pos:
+#         if num_lst[move_pos] < num:
+#             start_pos = move_pos + 1
+#         elif num_lst[move_pos] > num:
+#             end_pos = move_pos - 1
+#         else:
+#             return True
+#         move_pos = (start_pos + end_pos) // 2
+#
+#     return False
 
 
 n = int(input())
 
 num_list = list(map(int, input().split()))
-# print(num_list)
 
 num_list.sort()
 
@@ -37,7 +40,8 @@ m = int(input())
 check_list = list(map(int, input().split()))
 
 for ch in check_list:
-    if is_in_list(num_list, ch):
+    if is_in_list(ch, num_list, 0, len(num_list) - 1):
+    # if is_in_list(ch, num_list):
         print(1)
     else:
         print(0)
