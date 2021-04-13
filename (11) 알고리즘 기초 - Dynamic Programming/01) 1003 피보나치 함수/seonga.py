b7093bb7a8f0c8bcd3313b983c89c44a16848cc7
ssung <33725048+parkseonga@users.noreply.github.com>
2021-04-13 20:03:49 +0900

    Update seonga.py

--- (11) 알고리즘 기초 - Dynamic Programming/01) 1003 피보나치 함수/seonga.py
+++ (11) 알고리즘 기초 - Dynamic Programming/01) 1003 피보나치 함수/seonga.py
@@ -1,42 +1,12 @@
-import sys
+# Memorization방식으로 0, 1이 호출되는 횟수를 저장함.
+zero = [1, 0]
+one = [0, 1]
 
-def fibonacci(n):
+for i in range(2, 41):  # 40까지 
+    zero.append(zero[i-1]+zero[i-2])
+    one.append(one[i-1]+zero[i-2])
     
-    dict = {1:0, 0:0}    
-
-    count = 0
-    
-    current = 1
-    previous = 0
-
-        
-    for _ in range(0, n-1):
-        
-        count+=1
-        
-        current, previous = current+previous, current
-        
-    dict[1] = count        
-        
-    
-    if n>=2 or n==0:
-        
-        dict[0] = 1
-        
-    elif n == 1:
-        dict[1] = 1
-        
-    return dict
-
-N = int(input())
-lst = []
-
-for i in range(N):
-    
-    lst.append(int(input()))
- 
-for j in lst:
-    
-    ans = fibonacci(j)
-    
-    print(ans[0], ans[1])
+# index로 접근해서 출력
+for _ in range(int(input())):
+    n = int(input())
+    print(zero[n], zero[n])
