86d24a3402d9ae1b326949cb676b340c66f97ff0
ssung <33725048+parkseonga@users.noreply.github.com>
2021-04-17 20:58:16 +0900

    Update seonga.py

--- (11) 알고리즘 기초 - Dynamic Programming/03) 11726 2xn 타일링/seonga.py
+++ (11) 알고리즘 기초 - Dynamic Programming/03) 11726 2xn 타일링/seonga.py
@@ -3,10 +3,10 @@ N = int(input())  # 입력
 tile = [0 for _ in range(N)]  # 타일
  
 for i in range(N):
-    if i == 0:
+    if i == 0:  # 입력받은 N이 1일 때 -> 2x1 사각형
         tile[i] = 1
         continue
-    elif i == 1:
+    elif i == 1:  # 입력받은 N이 2일 때 -> 2x2 사각형
         tile[i] = 3
         continue
     tile[i] = tile[i-1] + tile[i-2]  # 점화식 적용 
