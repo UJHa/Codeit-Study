69973156dce9c0a3f23e9d5d38f87208dd9b7f9c
UHA <ddkk94@naver.com>
2021-05-03 22:28:48 +0900

    "신규 아이디 추천" 코드 수정 버전
    2단계 조건
    python에서 in 연산자를 통해서 '-', '_', '.' 모아서 처리
    3단계 조건
    문자열 replace 함수를 통하여 제거하도록 코드를 직관적으로 수정

--- 프로그래머스/(01) 2021 KAKAO BLIND RECRUITMENT/01) 신규 아이디 추천/jinhwan.py
+++ 프로그래머스/(01) 2021 KAKAO BLIND RECRUITMENT/01) 신규 아이디 추천/jinhwan.py
@@ -4,29 +4,24 @@ def get_remove_impossible_char(id):
     for s in id:
         if s.isalpha() \
                 or s.isdigit() \
-                or s == '-' \
-                or s == '_' \
-                or s == '.':
+                or s in ['-', '_', '.']:
             result += s
     return result
 
 
 def get_trim_dot(id):
     # 3단계 마침표 중복 제거
-    prev_str = ''
-    result = ''
-    for p_s in id:
-        if p_s != '.':
-            result += p_s
-        else:  # if p_s == '.':
-            if prev_str != p_s:
-                result += p_s
-        prev_str = p_s
+    while '..' in id:
+        id = id.replace('..', '.')
+
+    result = id
+
     # 4단계 마침표 맨 앞, 맨뒤 제거
     if result[0] == '.':
         result = result[1:]
     if len(result) > 1 and result[-1] == '.':
         result = result[:-1]
+
     return result
 
 
@@ -46,12 +41,12 @@ def solution(new_id):
         answer = 'a'
 
     # 6단계
-    if len(answer) >= 16:
+    if len(answer) > 15:
         answer = answer[:15]
-        while answer[-1] == '.':
+        if answer[-1] == '.':
             answer = answer[:-1]
 
     # 7단계
-    while len(answer) <= 2:
+    while len(answer) < 3:
         answer += answer[-1]
     return answer
