68139a721f33d89d8111737afa3b80a9feecc935
UHA <ddkk94@naver.com>
2021-05-05 22:02:11 +0900

    Update jinhwan.py
    list > dict로 코드 개선(불필요 함수 제거)
    거리 계산 방식을 좌표 간 거리가 아닌 상하좌우 이동으로 몇 회인지 비교

--- 프로그래머스/(02)2020 카카오 인턴십/01)키패드 누르기/jinhwan.py
+++ 프로그래머스/(02)2020 카카오 인턴십/01)키패드 누르기/jinhwan.py
@@ -1,51 +1,51 @@
-# 1차 풀이(정확도 60%로 실패)
-def get_num_pos(pos_num_list, number):
-    for y in range(len(pos_num_list)):
-        for x in range(len(pos_num_list[y])):
-            if number == pos_num_list[y][x]:
-                return [x, y]
-    return None
-
-
+# 2차 풀이(정확도 100% 성공)
 def solution(numbers, hand):
     answer = ''
-    pos_num_list = []
-    pos_num_list.append(['1', '2', '3'])
-    pos_num_list.append(['4', '5', '6'])
-    pos_num_list.append(['7', '8', '9'])
-    pos_num_list.append(['*', '0', '#'])
+    pos_num_dict = {}
+    x, y = 0, 0
+    for i in range(1, 12 + 1):
+        if i == 10:
+            pos_num_dict['*'] = [x, y]
+        elif i == 11:
+            pos_num_dict[0] = [x, y]
+        elif i == 12:
+            pos_num_dict['#'] = [x, y]
+        else:
+            pos_num_dict[i] = [x, y]
 
-    left_hand_pos = get_num_pos(pos_num_list, '*')
-    right_hand_pos = get_num_pos(pos_num_list, '#')
+        x, y = (x + 1) % 3, i // 3
+
+    left_pos = pos_num_dict['*']
+    right_pos = pos_num_dict['#']
 
     for n in numbers:
-        n = str(n)
-        if n in ['1', '4', '7']:
+        if n in [1, 4, 7]:
             answer += 'L'
-            left_hand_pos = get_num_pos(pos_num_list, n)
-            pass
-        elif n in ['3', '6', '9']:
+            left_pos = pos_num_dict[n]
+        elif n in [3, 6, 9]:
             # 369 right
             answer += 'R'
-            right_hand_pos = get_num_pos(pos_num_list, n)
-            pass
+            right_pos = pos_num_dict[n]
         else:  # 2, 5, 8, 0
             # 2580 조건
-            center_pos = get_num_pos(pos_num_list, n)
-            left_distance = (center_pos[0] - left_hand_pos[0]) ** 2 + (center_pos[1] - left_hand_pos[1]) ** 2
-            right_distance = (center_pos[0] - right_hand_pos[0]) ** 2 + (center_pos[1] - right_hand_pos[1]) ** 2
+            center_pos = pos_num_dict[n]
+            left_distance = abs(center_pos[0] - left_pos[0]) + abs(center_pos[1] - left_pos[1])
+            right_distance = abs(center_pos[0] - right_pos[0]) + abs(center_pos[1] - right_pos[1])
             if left_distance < right_distance:
                 answer += 'L'
-                left_hand_pos = center_pos
+                left_pos = center_pos
             elif left_distance > right_distance:
                 answer += 'R'
-                right_hand_pos = center_pos
+                right_pos = center_pos
             else:
                 if hand == 'left':
                     answer += 'L'
-                    left_hand_pos = center_pos
+                    left_pos = center_pos
                 elif hand == 'right':
                     answer += 'R'
-                    right_hand_pos = center_pos
+                    right_pos = center_pos
 
     return answer
+
+
+print(solution([1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5], 'right'))
