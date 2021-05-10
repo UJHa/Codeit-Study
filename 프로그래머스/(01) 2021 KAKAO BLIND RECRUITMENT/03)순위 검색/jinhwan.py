7cba9678fe3eb2b8fb09f75502ba5742e1ac08a9
UHA <ddkk94@naver.com>
2021-05-11 02:20:05 +0900

    순위 검색 문제 풀이 성공

--- 프로그래머스/(01) 2021 KAKAO BLIND RECRUITMENT/03)순위 검색/jinhwan.py
+++ 프로그래머스/(01) 2021 KAKAO BLIND RECRUITMENT/03)순위 검색/jinhwan.py
@@ -1,64 +1,40 @@
-# 3차 실패(정확성은 100%, 효율성 0%)
+# 4차 성공(정확성은 100%, 효율성 100%)
 # 1차 방식 : info 데이터 내부의 columns 하나씩 비교하는 방식
 # 2차 방식 : info의 column들을 가져와서 일치하는 데이터의 index에 접근하여 비교하는 방식
 # 3차 방식 : info의 점수를 뺀 나머지 데이터를 문자열 키로 dictionary 생성, query로 key를 만들어서 dict의 value(list)에서 점수가 query score이상인 갯수 반환 방식
+# 4차 방식
+# # 정렬(sort)를 query 실행 시점이 아니라 info_dict 생성 후 실행하도록 변경(query 배열의 최대 크기보다 info 배열의 최대 크기가 작음)
+# # 점수 조건에 일치하는 개수를 이진 탐색(lower bound)으로 변경
 
-import itertools
-
-
-def get_query_list(query):
-    result = []
-    query_list = [q.split() for q in query]
-    for q in query_list:
-        score = int(q[-1])
-        q_data = q[:-1]
-
-        query_key = ''
-        for data in q_data:
-            if data != 'and' and not data.isdigit():
-                query_key += data
-        result.append((query_key, score))
-    return result
+from itertools import combinations
+from bisect import bisect_left
 
 
 def make_info_dict(info):
     result = {}
-    info_list = [i.split() for i in info]
-    for i, v in enumerate(info_list):
-        score = int(v[-1])
-        data = v[:-1]
-        keys = get_info_key_list(data)
+    for _i in info:
+        val = _i.split()
+        score = int(val[-1])
+        data = val[:-1]
 
-        for k in keys:
-            if result.get(k) is None:
-                result[k] = [(i, score)]
-            else:
-                result[k].append((i, score))
+        for num in range(5):
+            for tu in combinations([0, 1, 2, 3], num):
+                key_string = make_info_key_string(data, tu)
+
+                if result.get(key_string) is None:
+                    result[key_string] = [score]
+                else:
+                    result[key_string].append(score)
     return result
 
 
-def get_info_key_list(data):
-    result = []
-    for i in range(5):
-        for j in itertools.combinations([0, 1, 2, 3], i):
-            temp_data = list(data)
-            for column in j:
-                temp_data[column] = '-'
-            result.append(''.join(temp_data))
-    return result
-
-
-def get_data_count(data_list, q_score):
-    result = 0
-    data_list.sort(key=lambda x: x[1], reverse=True)
-
-    for d in data_list:
-        d_score = d[1]
-        if q_score <= d_score:
-            result += 1
+def make_info_key_string(data, combine_tuple):
+    result = ''
+    for i in range(4):
+        if i in combine_tuple:
+            result += '-'
         else:
-            break
-
+            result += data[i]
     return result
 
 
@@ -66,16 +42,20 @@ def solution(info, query):
     answer = []
 
     info_dict = make_info_dict(info)
-    query_lists = get_query_list(query)
 
-    for q in query_lists:
-        q_key = q[0]
-        q_score = q[1]
-        count = 0
+    for val in info_dict.values():
+        val.sort()
+
+    for q in query:
+        q_data = q.split()
+        q_key = q_data[0] + q_data[2] + q_data[4] + q_data[6]
+        q_score = int(q_data[-1])
+
         if info_dict.get(q_key) is None:
             count = 0
         else:
-            count = get_data_count(info_dict[q_key], q_score)
+            data_list = info_dict[q_key]
+            count = len(data_list) - bisect_left(data_list, q_score)
         answer.append(count)
 
     return answer
