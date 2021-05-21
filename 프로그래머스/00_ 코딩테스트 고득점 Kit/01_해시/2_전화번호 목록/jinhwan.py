05fbc2e8380e4a9ccccc88f0cf17d2ff79b24423
UHA <ddkk94@naver.com>
2021-05-21 19:45:18 +0900

    전화번호 목록 3차 풀이 성공

--- 프로그래머스/00_ 코딩테스트 고득점 Kit/01_해시/2_전화번호 목록/jinhwan.py
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/01_해시/2_전화번호 목록/jinhwan.py
@@ -1,22 +1,28 @@
 # 1차 풀이 : 정확성 11,14번 실패, 효율성 성공
 # 2차 풀이 : 정확성 성공, 효율성 4번 실패
+# 3차 풀이 성공
+# # 2차 풀이에서 prefix_dict의 value를 list > dictionary로 변경
+# # 변경 이유 : list보다 dictionary의 key를 통해서 찾는 것이 빠름
 def solution(phone_book):
     answer = True
     prefix_dict = {}
 
     for p in phone_book:
         if prefix_dict.get(len(p)) is None:
-            prefix_dict[len(p)] = [p]
+            prefix_dict[len(p)] = {p: 1}
         else:
-            prefix_dict[len(p)].append(p)
+            if prefix_dict[len(p)].get(p) is None:
+                prefix_dict[len(p)][p] = 1
+            else:
+                prefix_dict[len(p)][p] += 1
 
     for key, value in prefix_dict.items():
         num_length = key
-        num_list = value
+        num_dict = value
         for p in phone_book:
             if len(p) <= num_length:
                 continue
-            if p[:num_length] in num_list:
+            if num_dict.get(p[:num_length]) is not None:
                 return False
 
     return answer
