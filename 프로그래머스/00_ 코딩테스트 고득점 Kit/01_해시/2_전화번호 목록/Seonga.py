2a2254e8e7b9d304c4578dce72fa299915d3b980
ssung <33725048+parkseonga@users.noreply.github.com>
2021-05-29 18:47:43 +0900

    Update Seonga.py

--- 프로그래머스/00_ 코딩테스트 고득점 Kit/01_해시/2_전화번호 목록/Seonga.py
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/01_해시/2_전화번호 목록/Seonga.py
@@ -7,20 +7,21 @@ def solution(phone_book):
 
     answer = True
     
-    hash_map = {}
+    hash_map = {}  # 전화번호를 담을 딕셔너리 
     
     for phone_number in phone_book:
         
-        hash_map[phone_number] = hash(phone_number)
+        hash_map[phone_number] = hash(phone_number)  # 전화번호에 해당하는 hash 값을 키로 value를 phone number로 딕셔너리 생성
         
-    for phone_number in phone_book:
+    for phone_number in phone_book: 
         
         temp = ""
         
         for number in phone_number:
             
-            temp += number
+            temp += number  # 접두사가 있는지 확인 
             
+            # 딕셔너리 내에 접두사가 있으면 False 반환 
             if temp in hash_map and temp != phone_number:
                 
                 answer = False
