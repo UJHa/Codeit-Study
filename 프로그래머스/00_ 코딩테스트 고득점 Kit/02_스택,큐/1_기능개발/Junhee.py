b422c6937ae8d9ec6b247ab8a6bd727ba2d9f4b5
JunheeLee <myopqr1234@gmail.com>
2021-06-05 19:27:00 +0900

    기능개발

---
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/02_스택,큐/1_기능개발/Junhee.py
@@ -0,0 +1,36 @@
+# 정확성 100 합계 100
+# progresses와 speeds 각각의 deque을 만들어 queue를 구현
+
+from collections import deque
+
+
+def solution(progresses, speeds):
+    answer = []
+    dq_pro = deque()
+    dq_pro.extend(progresses)
+    dq_spd = deque()
+    dq_spd.extend(speeds)
+    
+    while dq_pro:
+        ans = 0
+        # 각 작업 진도 더하기
+        for i, j in enumerate(dq_spd):
+            dq_pro[i] += j
+
+        # 배포
+        if dq_pro[0] >= 100:
+            while dq_pro:
+                if dq_pro[0] < 100:
+                    break
+                dq_pro.popleft()
+                dq_spd.popleft()
+                ans += 1
+                
+        if ans:
+            answer.append(ans)
+    
+    return answer
+
+
+print(solution([93, 30, 55], [1, 30, 5]))
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
