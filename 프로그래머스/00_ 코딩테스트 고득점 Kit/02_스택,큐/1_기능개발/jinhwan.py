0ff236fdd0c4488edcfb8e0459516a3dcb876e3a
UHA <ddkk94@naver.com>
2021-06-05 10:50:45 +0900

    기능개발 문제 풀이 성공

--- 프로그래머스/00_ 코딩테스트 고득점 Kit/02_스택,큐/1_기능개발/jinhwan.py
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/02_스택,큐/1_기능개발/jinhwan.py
@@ -1,3 +1,23 @@
+# 기능개발 (성공)
+# 정확성: 100.0
+# 합계: 100.0 / 100.0
+import math
+
+
 def solution(progresses, speeds):
     answer = []
+
+    prev_days = 0
+    for p, s in zip(progresses, speeds):
+        work_days = math.ceil((100 - p) / s)
+        if work_days > prev_days:
+            answer.append(1)
+            prev_days = work_days
+        else:
+            answer[-1] += 1
+
     return answer
+
+
+s = solution([93, 30, 55], [1, 30, 5])  # [2, 1]
+print(s)
