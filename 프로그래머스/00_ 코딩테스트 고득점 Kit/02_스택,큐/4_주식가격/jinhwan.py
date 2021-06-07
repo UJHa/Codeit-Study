f99eb036af64580a7dabd4e907de71d724cd39d7
UHA <ddkk94@naver.com>
2021-06-07 22:36:27 +0900

    주식가격 풀이 테스트 코드
    정확성 통과
    효율성 실패(시간 초과)

--- 프로그래머스/00_ 코딩테스트 고득점 Kit/02_스택,큐/4_주식가격/jinhwan.py
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/02_스택,큐/4_주식가격/jinhwan.py
@@ -1,19 +1,25 @@
 # 주식가격(실패)
-# 정확성: 6.7
-# 효율성: 0.0
-# 합계: 6.7 / 100.0
+# 정확성: 66.7(10/10)
+# 효율성: 0.0(5/5) 시간 초과
+# 합계: 66.7 / 100.0
 def solution(prices):
     answer = []
-
+    prices_count = len(prices)
     for i, p in enumerate(prices):
-        count = 0   # 가격이 떨어지지 않은 기간을 저장하는 변수
-        for rp in prices[i+1:]:
-            if p <= rp:
-                count += 1
-        answer.append(count)
+        is_consecutive = True
+        end_index = 0
+        for j, rp in zip(range(i + 1, prices_count), prices[i + 1:]):
+            if rp < p:
+                is_consecutive = False
+                end_index = j
+                break
+        if is_consecutive:
+            answer.append(prices_count - 1 - i)
+        else:
+            answer.append(end_index - i)
 
     return answer
 
 
-s = solution([1, 2, 3, 2, 3])   # [4, 3, 1, 1, 0]
+s = solution([1, 2, 3, 2, 3])  # [4, 3, 1, 1, 0]
 print(s)
