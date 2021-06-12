109753bc64273ab000cd75cd2546b8fc6ad7cde3
UHA <ddkk94@naver.com>
2021-06-12 20:24:51 +0900

    가장 큰 수 풀이 코드(성공)

--- 프로그래머스/00_ 코딩테스트 고득점 Kit/04_정렬/2_가장_큰_수/jinhwan.py
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/04_정렬/2_가장_큰_수/jinhwan.py
@@ -1,23 +1,21 @@
-# 가장 큰 수(실패)
-# 정확성: 27.3
-# 합계: 27.3 / 100.0
-import heapq
-
-
+# 가장 큰 수(성공)
+# 정확성: 100.0
+# 합계: 100.0 / 100.0
 def solution(numbers):
-    answer = ''
-    max_number_length = 4 # 1000
-    num_heap = []
-    for n in numbers:
-        compare_num = -n * 10 ** (max_number_length - len(str(n)))
-        heapq.heappush(num_heap, (compare_num, n))
+    l = []
+    max_length = 4
+    for number in numbers:
+        origin_num = str(number)
+        # 정렬을 위해 4자리의 숫자로 변환
+        key_num = origin_num * max_length
+        key_num = int(key_num[:max_length])
 
-    while num_heap:
-        num = heapq.heappop(num_heap)[1]
-        answer += str(num)
-    return answer
+        l.append([key_num, origin_num])
 
-# 2 * (i + 1) - 1, 2 * (i + 1)
+    l = sorted(l, reverse=True)
+    return str(int("".join([item[1] for item in l])))
 
-s = solution([6, 10, 2])  # "6210"
+
+# s = solution([6, 10, 2])  # "6210"
+s = solution([3, 30, 34, 5, 9])  # "9534330"
 print(s)
