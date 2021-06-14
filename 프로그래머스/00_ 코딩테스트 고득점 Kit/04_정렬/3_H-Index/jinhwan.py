385d951ced13064becf5f866851b2ce8a2845612
UHA <ddkk94@naver.com>
2021-06-14 22:21:20 +0900

    H-Index 풀이 코드(속도 개선)

--- 프로그래머스/00_ 코딩테스트 고득점 Kit/04_정렬/3_H-Index/jinhwan.py
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/04_정렬/3_H-Index/jinhwan.py
@@ -2,23 +2,15 @@
 # 정확성: 100.0
 # 합계: 100.0 / 100.0
 def solution(citations):
-    answer = 0
-    for index in range(len(citations)):
-        up_count = 0
-        down_count = 0
-        for c in range(citations):
-            if index <= c:
-                up_count += 1
-            if index >= c:
-                down_count += 1
-
-        if down_count <= index <= up_count:
-            answer = index
-        index += 1
-    return answer
+    sort_list = sorted(citations)
+    _length = len(sort_list)
+    for index in range(_length):
+        if citations[index] >= _length - index:
+            return _length - index
+    return 0
 
 
-# s = solution([3, 0, 6, 1, 5])  # 3
+s = solution([3, 0, 6, 1, 5])  # 3
 # s = solution([7, 2, 9, 8, 5])  # 7
-s = solution([22, 42])  # 3
+# s = solution([22, 42])  # 3
 print(s)
