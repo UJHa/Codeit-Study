cdffadf4c5be5ea4a4b238e48bfbce19af0525f3
JunheeLee <myopqr1234@gmail.com>
2021-06-19 17:13:52 +0900

    더_맵게

---
+++ 프로그래머스/00_ 코딩테스트 고득점 Kit/03_힙 - Heap/1_더_맵게/Junhee.py
@@ -0,0 +1,24 @@
+# 최소 힙을 구현하여 푼다
+import heapq
+
+
+def solution(scoville, K):
+    answer = 0
+    heapq.heapify(scoville)
+    
+    while len(scoville) >= 2:
+        # 모든 음식이 최소 K 이상의 스코빌 지수인가?
+        hot = heapq.heappop(scoville)
+        if hot >= K:
+            break
+        
+        # 섞기
+        second_hot = heapq.heappop(scoville)
+        heapq.heappush(scoville, hot+second_hot*2)
+        answer += 1
+    
+    # 출력
+    if scoville[0] >= K:
+        return answer
+    else:
+        return -1
