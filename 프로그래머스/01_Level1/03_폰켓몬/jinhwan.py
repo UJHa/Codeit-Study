422adc9908f520a2cd6efb396967774087791b31
UHA <ddkk94@naver.com>
2021-06-24 09:30:44 +0900

    프로그래머스 폰켓몬 풀이 실패

--- 프로그래머스/01_Level1/03_폰켓몬/jinhwan.py
+++ 프로그래머스/01_Level1/03_폰켓몬/jinhwan.py
@@ -1,4 +1,18 @@
-# 폰켓몬
+# 폰켓몬(실패)
+# 06/24 09:10~
+# 정확성: 30.0
+# 합계: 30.0 / 100.0
+from itertools import combinations
+
+
 def solution(nums):
     answer = 0
+    for i in combinations(nums, len(nums) // 2):
+        answer = max(answer, len(set(i)))
     return answer
+
+
+# s = solution([3, 1, 2, 3]) == 2
+# s = solution([3, 3, 3, 2, 2, 4]) == 3
+s = solution([3, 3, 3, 2, 2, 2]) == 2
+print(s)
