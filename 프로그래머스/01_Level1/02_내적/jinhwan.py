eabf6a4eafd9fa1b0f3d625da86fa7f4f5e54425
UHA <ddkk94@naver.com>
2021-06-24 09:07:21 +0900

    내적 풀이

--- 프로그래머스/01_Level1/02_내적/jinhwan.py
+++ 프로그래머스/01_Level1/02_내적/jinhwan.py
@@ -1,5 +1,12 @@
 # 내적
 def solution(a, b):
-    answer = 1234567890
+    answer = 0
+    for x, y in zip(a, b):
+        answer += x * y
     return answer
-    
+    # return sum([x * y for x, y in zip(a, b)])
+
+
+# s = solution([1, 2, 3, 4], [-3, -1, 0, 2]) == 3
+s = solution([-1, 0, 1], [1, 0, -1]) == -2
+print(s)
