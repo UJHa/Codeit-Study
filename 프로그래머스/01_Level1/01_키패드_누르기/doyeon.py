ee0da49e7c2b9674697c2ac6d8c4d3a713d0077c
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2021-06-26 20:25:49 +0900

    Update doyeon.py

--- 프로그래머스/01_Level1/01_키패드_누르기/doyeon.py
+++ 프로그래머스/01_Level1/01_키패드_누르기/doyeon.py
@@ -1,7 +1,7 @@
 def solution(numbers, hand):
     answer = ''
 
-    # 키패드 좌표료 변경
+    # 키패드 좌표화
     dic = {1: [0, 0], 2: [0, 1], 3: [0, 2],
            4: [1, 0], 5: [1, 1], 6: [1, 2],
            7: [2, 0], 8: [2, 1], 9: [2, 2],
@@ -13,22 +13,22 @@ def solution(numbers, hand):
 
     for i in numbers:
         now = dic[i]
-        # 1, 4, 7을 누르는 경우 무조건 왼손
+        # 1, 4, 7을 누르는 경우는 왼손으로
         if i in [1, 4, 7]:
             answer += 'L'
             left_s = now
 
-        # 3, 6, 9를 누르는 경우 무조건 오른손
+        # 3, 6, 9를 누르는 경우는 오른손으로
         elif i in [3, 6, 9]:
             answer += 'R'
             right_s = now
 
-        # 2, 5, 8, 0을 누르는 경우
+        # 2, 5, 8, 0
         else:
             left_d = 0
             right_d = 0
 
-            # 좌표 거리 계산해주기
+            # 거리 계산
             for a, b, c in zip(left_s, right_s, now):
                 left_d += abs(a-c)
                 right_d += abs(b-c)
@@ -43,14 +43,13 @@ def solution(numbers, hand):
                 answer += 'R'
                 right_s = now
 
-            # 두 거리가 같은 경우
             else:
-                # 왼손잡이 경우
+                # 왼손잡이
                 if hand == 'left':
                     answer += 'L'
                     left_s = now
 
-                # 오른손잡이 경우
+                # 오른손잡이
                 else:
                     answer += 'R'
                     right_s = now
