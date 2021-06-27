bc19fc2fe97b239d1df193db02f2614a331ba456
UHA <ddkk94@naver.com>
2021-06-27 15:35:22 +0900

    K번째수 문제 풀이 코드

--- 프로그래머스/01_Level1/06_K번째수/jinhwan.py
+++ 프로그래머스/01_Level1/06_K번째수/jinhwan.py
@@ -1,4 +1,12 @@
 # K번째 수
 def solution(array, commands):
     answer = []
+    for c in commands:
+        i, j, k = c
+        temp = array[i-1:j]
+        answer.append(sorted(temp)[k-1])
     return answer
+
+
+s = solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]) == [5, 6, 3]
+print(s)
