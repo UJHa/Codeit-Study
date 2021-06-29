0a6f150bf7449a36d85eced0c20ab1cd54440b66
UHA <ddkk94@naver.com>
2021-06-29 18:23:01 +0900

    멀쩡한 사각형 코드(성공)
    y절편 이용한 풀이

--- 프로그래머스/02_Level2/02_멀쩡한_사각형/jinhwan.py
+++ 프로그래머스/02_Level2/02_멀쩡한_사각형/jinhwan.py
@@ -1,8 +1,25 @@
 # 멀쩡한 사각형
-def solution(s):
-    answer = -1
+# 정확성: 100.0
+# 합계: 100.0 / 100.0
+import math
 
-    # [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-    print('Hello Python')
 
+def solution(w, h):
+    if w == 1 or h == 1:
+        return 0
+    if w == h:
+        return w * h - w
+
+    start_y, end_y = 0, 0
+    not_use_count = 0
+    for x in range(1, w+1):
+        end_y = h * x / w
+        not_use_count += int(math.ceil(end_y) - (start_y // 1))
+        start_y = end_y
+
+    answer = w * h - not_use_count
     return answer
+
+
+s = solution(8, 12) == 80
+print(s)
