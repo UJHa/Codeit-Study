90371ca4198b7e0cf9f935f7d3d6516ac836a480
UHA <ddkk94@naver.com>
2021-06-29 16:43:35 +0900

    짝지어 제거하기 코드(실패2)

--- 프로그래머스/02_Level2/01_짝지어_제거하기/jinhwan.py
+++ 프로그래머스/02_Level2/01_짝지어_제거하기/jinhwan.py
@@ -1,17 +1,20 @@
-# 짝지어 제거하기(실패)
-# 정확성: 30.3
+# 짝지어 제거하기(실패2)
+# 정확성: 60.2
 # 효율성: 0.0
+# 합계: 60.2 / 100.0
 def solution(s):
+    l = list(s)
     check_index = 0
-    while len(s) > 1:
-        if s[check_index] != s[check_index+1]:
+    while len(l) > 1:
+        if l[check_index] != l[check_index + 1]:
             check_index += 1
-        if check_index == len(s) - 1:
+        if check_index == len(l) - 1:
             break
-        if s[check_index] == s[check_index+1]:
-            s = s[:check_index] + s[check_index+2:]
-            check_index = 0
-    if len(s) == 0:
+        if l[check_index] == l[check_index + 1]:
+            l.pop(check_index)
+            l.pop(check_index)
+            check_index = check_index > 0 and check_index - 1 or 0
+    if len(l) == 0:
         answer = 1
     else:
         answer = 0
