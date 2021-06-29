83ebde6ff5bd80d1eaca31c389c09dd4f921fa92
UHA <ddkk94@naver.com>
2021-06-29 17:02:36 +0900

    짝지어 제거하기 코드(성공)

--- 프로그래머스/02_Level2/01_짝지어_제거하기/jinhwan.py
+++ 프로그래머스/02_Level2/01_짝지어_제거하기/jinhwan.py
@@ -1,20 +1,17 @@
-# 짝지어 제거하기(실패2)
+# 짝지어 제거하기(성공) : 스택을 활용한 풀이
 # 정확성: 60.2
-# 효율성: 0.0
-# 합계: 60.2 / 100.0
+# 효율성: 39.8
+# 합계: 100.0 / 100.0
 def solution(s):
-    l = list(s)
-    check_index = 0
-    while len(l) > 1:
-        if l[check_index] != l[check_index + 1]:
-            check_index += 1
-        if check_index == len(l) - 1:
-            break
-        if l[check_index] == l[check_index + 1]:
-            l.pop(check_index)
-            l.pop(check_index)
-            check_index = check_index > 0 and check_index - 1 or 0
-    if len(l) == 0:
+    stack = []
+    for c in s:
+        if len(stack) > 0 and stack[-1] == c:
+            stack.pop()
+        else:
+            stack.append(c)
+    print(stack)
+    answer = -1
+    if len(stack) == 0:
         answer = 1
     else:
         answer = 0
