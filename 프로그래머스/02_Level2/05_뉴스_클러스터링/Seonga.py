bae51714a3beb2cef0e4045b8428e7b8ea247cbf
ssung <33725048+parkseonga@users.noreply.github.com>
2021-07-16 18:28:28 +0900

    Update Seonga.py

--- 프로그래머스/02_Level2/05_뉴스_클러스터링/Seonga.py
+++ 프로그래머스/02_Level2/05_뉴스_클러스터링/Seonga.py
@@ -1,4 +1,3 @@
-# 3번째 test case 성공 못함 
 def make_str(str_):
     
     str_lst = []
@@ -17,16 +16,17 @@ def solution(str1, str2):
     str1_lst = make_str(str1.lower())
     str2_lst = make_str(str2.lower())
 
-    union_len = len(set(str1_lst).union(set(str2_lst)))
-    inter_len = len(set(str1_lst).intersection(set(str2_lst)))
+    if len(str1_lst) > len(str2_lst):
+        inter_lst = [str1_lst.remove(x) for x in str2_lst if x in str1_lst]
+    else:
+        inter_lst = [str2_lst.remove(x) for x in str1_lst if x in str2_lst]
+
+    union_len = len(str1_lst + str2_lst)
 
     if union_len == 0:
-        union_len  = 1
-
-    if inter_len == 0:
-        inter_len = 1
+        return 65536
         
-    return round((inter_len/union_len)*65536)
+    return int((len(inter_lst)/union_len)*65536)
 
 solution("FRANCE", "french")
 solution("handshake", "shake hands")
