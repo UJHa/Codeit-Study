a08e751d27ec9a47d7c77b06afb5fbf89b173f8b
ssung <33725048+parkseonga@users.noreply.github.com>
2021-07-30 03:19:06 +0900

    가장 큰 수 만들기 성공

--- 프로그래머스/02_Level2/13_큰_수_만들기/Seonga.py
+++ 프로그래머스/02_Level2/13_큰_수_만들기/Seonga.py
@@ -1,6 +1,5 @@
-
 def solution(number, k):
-    
+
     stack = [number[0]]
 
     for num in number[1:]:
@@ -12,6 +11,11 @@ def solution(number, k):
             stack.pop()  # 기존에 있는 값 제거 
 
         stack.append(num)  
+
+    # k = 1 이고 stack 에 쌓이는 값이 항상 첫번째 인덱스가 가장 크다면 while문을 돌지 않으므로 
+    if k != 0:
+        for _ in range(k):
+            stack.pop()
 
     answer = ''.join(stack)
 
