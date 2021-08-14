68b55f1732a699108a4aeeeb3075c4e27ec5a574
doyeonk429 <80318425+doyeonk429@users.noreply.github.com>
2021-08-14 18:59:26 +0900

    Update dy.py

--- 프로그래머스/02_Level2/08_괄호_변환/dy.py
+++ 프로그래머스/02_Level2/08_괄호_변환/dy.py
@@ -3,22 +3,22 @@ def solution(s):
     cor = {'(':')', '{':'}', '[':']'}
     
     for i in range(len(s)): # x(0 ≤ x < (s의 길이)) 만큼 반복
-        right = list(s[i:] + s[:i]) # 회전한 리스트
-        left = [] # 빈 리스트
+        new = list(s[i:] + s[:i]) # 회전한 리스트
+        check = [] # 빈 리스트
         
-        while right:
-            temp = right.pop(0)
-            if not left:
-                left.append(temp) # temp 추가
+        while new:
+            temp = new.pop(0)
+            if not check:
+                check.append(temp) # temp 추가
             else:
-                if left[-1] in cor.values(): # 닫힌 쪽이면 break
+                if check[-1] in cor.values(): # 닫힌 쪽이면 break
                     break
-                if cor[left[-1]] == temp: # 닫힌 쪽 아니고, temp와 동일하면 제거
-                    left.pop()
+                if cor[check[-1]] == temp: # 닫힌 쪽 아니고, temp와 동일하면 제거
+                    check.pop()
                 else: # 닫힌 쪽 아니고, temp와 다르면 추가
-                    left.append(temp)
+                    check.append(temp)
                     
-        if not left: # 다 지운 경우 최종 answer +1
+        if not check: # 다 지운 경우 최종 answer +1
             answer += 1
     
     return answer
